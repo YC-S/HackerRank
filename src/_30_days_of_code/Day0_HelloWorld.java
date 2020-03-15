2aa31a51ed60208c662dc39b75efdd5776fa62ac
yuanchen <yuanchenshi@gmail.com>
2020-03-14 21:10:21 -0500

    Day0_HelloWorld

---
+++ src/_30_days_of_code/Day0_HelloWorld.java
@@ -0,0 +1,13 @@
+package _30_days_of_code;
+
+import java.util.Scanner;
+
+public class Day0_HelloWorld {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    System.out.println("Hello, World.");
+    System.out.println(str);
+  }
+}
